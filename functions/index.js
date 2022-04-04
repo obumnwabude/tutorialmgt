797ab485c8e1ad9ca9af7a7d6a87693154fb8a6f
Obumuneme Nwabude <obumnwabude@gmail.com>
2022-04-04 13:19:26 +0100

    chore: configure cloud functions for user and session counters
    create and delete users in firestore in sync with auth.
    deploy cloud functions.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,61 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createFirestoreUser = functions.auth
+  .user()
+  .onCreate(async (authUser) => {
+    await db
+      .doc('/users/counter')
+      .set({ count: admin.firestore.FieldValue.increment(1) }, { merge: true })
+      .catch((error) => console.error(error));
+
+    await db
+      .doc(`/users/${authUser.uid}`)
+      .set(
+        { authInfo: { email: authUser.email, uid: authUser.uid } },
+        { merge: true }
+      )
+      .catch((error) => console.error(error));
+  });
+
+exports.deleteFirestoreUser = functions.auth
+  .user()
+  .onDelete(async (authUser) => {
+    await db
+      .doc('/users/counter')
+      .set({ count: admin.firestore.FieldValue.increment(-1) }, { merge: true })
+      .catch((error) => console.error(error));
+
+    await db
+      .doc(`/users/${authUser.uid}`)
+      .delete()
+      .catch((error) => console.error(error));
+  });
+
+exports.increaseSessionCount = functions.firestore
+  .document('/sessions/{sessionId}')
+  .onCreate(
+    async (_, __) =>
+      await db
+        .doc('/sessions/counter')
+        .set(
+          { count: admin.firestore.FieldValue.increment(1) },
+          { merge: true }
+        )
+        .catch((error) => console.error(error))
+  );
+
+exports.reduceSessionCount = functions.firestore
+  .document('/sessions/{sessionId}')
+  .onDelete(
+    async (_, __) =>
+      await db
+        .doc('/sessions/counter')
+        .set(
+          { count: admin.firestore.FieldValue.increment(-1) },
+          { merge: true }
+        )
+        .catch((error) => console.error(error))
+  );
