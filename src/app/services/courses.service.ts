ca9fc96bd99b8addec7f49f3df8b30f185360368
Obumuneme Nwabude <obumnwabude@gmail.com>
2022-04-13 12:23:20 +0100

    fix: convert onSnapshot to getDoc for courses service
    did this for faster loading and to reduce reads.
    After all the courses will not be frequently updated.
    add ApplicationRef.tick() when the courses are fetched too.

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,25 +1,32 @@
-import { Injectable } from '@angular/core';
-import { doc, Firestore, getDoc, onSnapshot } from '@angular/fire/firestore';
+import { ApplicationRef, Injectable } from '@angular/core';
+import { doc, Firestore, getDoc } from '@angular/fire/firestore';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { of } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
   all = of([]);
-  constructor(private firestore: Firestore, private snackBar: MatSnackBar) {
-    onSnapshot(doc(this.firestore, '/resources/courses'), {
-      next: (snap) =>
-        (this.all = snap.exists() ? of(snap.data()['all'] ?? []) : of([])),
-      error: (error) =>
-        this.snackBar
-          .open(error.message, 'REFRESH PAGE', {
-            panelClass: ['snackbar-error'],
-            horizontalPosition: 'center'
-          })
-          .onAction()
-          .subscribe(() => window.location.reload())
-    });
+  constructor(
+    private firestore: Firestore,
+    private appRef: ApplicationRef,
+    private snackBar: MatSnackBar
+  ) {
+    getDoc(doc(this.firestore, '/resources/courses'))
+      .then((snap) => {
+        this.all = snap.exists() ? of(snap.data()['all'] ?? []) : of([]);
+        this.appRef.tick();
+      })
+      .catch((error) => {
+        firstValueFrom(
+          this.snackBar
+            .open(error.message, 'REFRESH PAGE', {
+              panelClass: ['snackbar-error'],
+              horizontalPosition: 'center'
+            })
+            .onAction()
+        ).then(() => window.location.reload());
+      });
   }
 }
